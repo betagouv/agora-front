415ed7c82f707bc42443c5869627253103a86afd
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-09-21 17:22:19 +0200

    MAJ mentions légales ⚖️

--- client/pages/mentions-legales/index.vue
+++ client/pages/mentions-legales/index.vue
@@ -10,46 +10,66 @@ useHead({
 })
 
 const domainName = useRuntimeConfig().public.domainName
-const links: Ref<any[]> = ref([{ to: '/', text: 'Accueil' }, { text: 'Mentions légales' }])
+const links: Ref<any[]> = ref([{to: '/', text: 'Accueil'}, {text: 'Mentions légales'}])
 </script>
 
 <template>
-  <DsfrBreadcrumb :links="links" />
+  <DsfrBreadcrumb :links="links"/>
   <div class="fr-mt-8w">
     <h1>Mentions légales</h1>
     <h2>Éditeur</h2>
     <p>
-      Agora est édité par la Fabrique Numérique du Ministère de la Transition écologique et solidaire et du Ministère de la
-      Cohésion
-      des territoires et des Relations avec les collectivités territoriales (Grande Arche de la Défense, 92055 La Défense CEDEX), avec
-      l’appui de <a
-      href="https://beta.gouv.fr/"
-      target="_blank"
-      rel="external"
-    >l’incubateur de services numériques beta.gouv.fr</a> de la
-      direction interministérielle du numérique (DINUM).
+      Agora est édité par :<br/>
+      La Direction interministérielle de la transformation publique (DITP) :<br/>
+      Ensemble Fontenoy Ségur - 20 avenue de Ségur<br/>
+      75007 Paris<br/>
+      01 79 84 33 00
     </p>
     <h2>Directeur de la publication</h2>
-    <ul>
-      <li>Monsieur Brice HUET, directeur général par intérim de l'aménagement, du logement et de la nature (DGALN)</li>
-      <li>Adresse : Tour Séquoïa 1, place Carpeaux 92055 La Défense Cedex</li>
-    </ul>
-    <p />
-    <h2>Hébergement</h2>
     <p>
-      Le site Agora ({{ domainName }}) est hébergé par la société Scalingo SAS,
-      inscrite au RCS (Strasbourg B 808 665 483) et dont les serveurs se situent en France.
-      <br>
-      SIREN : 808665483
-      <br>
-      Siège social :15 avenue du Rhin, 67100 Strasbourg, France.
+      Le directeur de la publication est Thierry Lambert, délégué interministériel de la transformation publique.
+    </p>
+    <h2>Hébergement de l'applicatoin</h2>
+    <p>
+      Cette application est hébergée en propre par :<br/>
+      Scalingo<br/>
+      <a href="https://scalingo.com/fr/"
+         target="_blank" rel="external">https://scalingo.com/fr/</a><br/>
+      15 avenue du Rhin<br/>
+      67100 Strasbourg<br/>
+      France<br/>
     </p>
     <h2>Accessibilité</h2>
     <p>
-      Voir la page
-      <router-link to="/accessibilite">
-        Déclaration d'accessibilité
-      </router-link>
+      La conformité aux normes d'accessibilité numérique est en cours. L'objectif de rendre cette application accessible à toutes et tous
+      est une priorité.
+    </p>
+    <h2>En savoir plus</h2>
+    <p>
+      Pour en savoir plus sur la politique d’accessibilité numérique de l’État : <a
+      href="http://references.modernisation.gouv.fr/accessibilite-numerique"
+      target="_blank" rel="external">http://references.modernisation.gouv.fr/accessibilite-numerique</a>
+    </p>
+    <h2>Signaler un dysfonctionnement</h2>
+    <p>
+      Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité de l’application, merci de
+      nous en faire part à l’e-mail où nous tâcherons de vous répondre dans les plus brefs délais : agora@beta.gouv.fr<br/>
+      Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en droit de faire parvenir vos doléances ou une demande de saisine au
+      Défenseur des droits.<br/><br/>
+      Pour le joindre, vous pouvez :
+      <ul>
+        <li>Utiliser le formulaire de contact en ligne ici : <a
+          href="https://formulaire.defenseurdesdroits.fr/code/afficher.php?ETAPE=accueil_2016"
+          target="_blank" rel="external">https://formulaire.defenseurdesdroits.fr/code/afficher.php?ETAPE=accueil_2016</a></li>
+        <li>Composer le 09 69 39 00 00 (du lundi au vendredi de 8h à 20h)</li>
+        <li>Envoyer un courrier (sans timbre) à l’adresse suivante : Défenseur des droits, Libre réponse 71120, 75342 Paris CEDEX 07.</li>
+      </ul>
+    </p>
+    <h2>Sécurite</h2>
+    <p>
+      L’application est protégée par un certificat électronique, matérialisé pour la grande majorité des navigateurs par un cadenas. Cette
+      protection participe à la confidentialité des échanges. En aucun cas les services associés à l’application ne seront à l’origine
+      d’envoi de courriels pour demander la saisie d’informations personnelles.
     </p>
   </div>
 </template>
