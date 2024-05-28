3c816675ec25c0d060d39c57495de9ca4e24f125
Jordan Dullier <jordan.dullier29@gmail.com>
2024-05-28 15:57:09 +0200

    fix middleware et route

--- client/middleware/redirect.global.ts
+++ client/middleware/redirect.global.ts
@@ -1,7 +1,7 @@
 import type { RouteLocationNormalized } from "~/.nuxt/vue-router-stub";
 
 export default defineNuxtRouteMiddleware((to: RouteLocationNormalized, _from: RouteLocationNormalized) => {
-  if (to.path.includes('/qags/') || to.path.includes('/consultations/')) {
+  if (to.path.includes('/consultations/')) {
     return navigateTo('/');
   }
 })
