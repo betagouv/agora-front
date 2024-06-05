f23b0e9ba2b296946fbe0c31901cf145f5de8b12
Jordan Dullier <jordan.dullier29@gmail.com>
2024-06-05 11:10:34 +0200

    Retrait du middleware

--- client/middleware/redirect.global.ts
+++
@@ -1,7 +0,0 @@
-import type { RouteLocationNormalized } from "~/.nuxt/vue-router-stub";
-
-export default defineNuxtRouteMiddleware((to: RouteLocationNormalized, _from: RouteLocationNormalized) => {
-  if (to.path.includes('/consultations/')) {
-    return navigateTo('/');
-  }
-})
