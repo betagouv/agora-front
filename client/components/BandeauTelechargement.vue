3fa7f15d5e2bd6eda76a6c254907f58ad317e6a7
Jordan Dullier <jordan.dullier29@gmail.com>
2024-06-20 17:47:40 +0200

    fix mise en forme

---
+++ client/components/BandeauTelechargement.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import {Ref} from "vue";
+
+const platformRef : Ref<string| null> = ref('desktop')
+const isMobileRef = ref(false)
+
+onMounted(()=>{
+  const userAgent = navigator.userAgent
+  if (/android/i.test(userAgent)) {
+    platformRef.value = 'android'
+    isMobileRef.value = true
+  }
+
+  if (/iPad|iPhone|iPod/.test(userAgent)) {
+    platformRef.value = "iOS";
+    isMobileRef.value = true
+  }
+})
+</script>
+
+<template>
+  <div class="fr-grid-row fr-grid-row--middle fr-grid-row--center fr-grid-row--gutters">
+    <div class="fr-col-12 fr-col-md-8">
+      <div class="text-grey fr-highlight fr-highlight--orange-terre-battue">
+        <slot/>
+        <div class="fr-mt-1w fr-grid-row fr-grid-row--middle fr-grid-row--gutters fr-px-md-6w">
+          <div v-if="!isMobileRef || platformRef == 'iOS'" class="fr-col-12 fr-col-lg-6">
+            <a class="fr-btn fr-btn--secondary" href="https://apps.apple.com/app/6449599025" target="_blank"
+               rel="noopener" title="Télécharger sur l’AppStore - nouvelle fenêtre">
+              <VIcon name="agora-apple" class="fr-mr-1w" />
+              Télécharger sur l’AppStore
+            </a>
+            <div v-if="platformRef=='desktop'" class="qr-code fr-my-2w">
+              <img
+                alt="QR code Agora AppStore"
+                src="/qrCodes/qr-code-ios.png"
+              />
+            </div>
+          </div>
+
+          <div v-if="!isMobileRef || platformRef == 'android'" class="fr-col-12 fr-col-lg-6">
+            <a class="fr-btn fr-btn--secondary" href="https://play.google.com/store/apps/details?id=fr.gouv.agora"
+               target="_blank" rel="noopener" title="Télécharger sur GooglePlay - nouvelle fenêtre">
+              <VIcon name="agora-google" class="fr-mr-1w" />
+              Télécharger sur GooglePlay
+            </a>
+            <div v-if="platformRef=='desktop'" class="qr-code fr-my-2w">
+              <img
+                alt="QR code Agora Google Play"
+                src="/qrCodes/qr-code-android.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="fr-col-12 fr-col-md-4">
+      <img src="/smartphones_complet.png" style="width: 70%;
+        max-width: 100vw;">
+    </div>
+  </div>
+
+
+</template>
+
+<style>
+.qr-code{
+  display: flex;
+  justify-content: center;
+  
+  > img{
+    max-width: 150px;
+  }
+}
+</style>
