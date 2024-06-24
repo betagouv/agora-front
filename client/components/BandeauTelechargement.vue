9e50d4191addbc9c0dfd9746ae751eb333bb4cc8
Jordan Dullier <jordan.dullier29@gmail.com>
2024-06-24 11:11:02 +0200

    Changement Qr code

--- client/components/BandeauTelechargement.vue
+++ client/components/BandeauTelechargement.vue
@@ -33,7 +33,7 @@ onMounted(()=>{
             <div v-if="platformRef=='desktop'" class="qr-code fr-my-2w">
               <img
                 alt="QR code Agora AppStore"
-                src="/qrCodes/qr-code-ios.png"
+                src="/qrCodes/qr-code-ios-fond-blanc.png"
               />
             </div>
           </div>
@@ -47,7 +47,7 @@ onMounted(()=>{
             <div v-if="platformRef=='desktop'" class="qr-code fr-my-2w">
               <img
                 alt="QR code Agora Google Play"
-                src="/qrCodes/qr-code-android.png"
+                src="/qrCodes/qr-code-android-fond-blanc.png"
               />
             </div>
           </div>
