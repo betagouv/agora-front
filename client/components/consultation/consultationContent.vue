f661a5a5c8728b5f18e5c41549855c6c2e1897ee
Jordan Dullier <jordan.dullier29@gmail.com>
2024-06-28 16:01:37 +0200

    fix link and creation composant consultationContent

---
+++ client/components/consultation/consultationContent.vue
@@ -0,0 +1,159 @@
+<script setup lang="ts">
+import Consultation from "~/client/types/consultation/consultation";
+import svgBook from "@gouvfr/dsfr/dist/artwork/pictograms/leisure/book.svg";
+
+defineProps<{
+  consultation: Consultation
+}>()
+</script>
+
+<template>
+  <div class="fr-grid fr-mb-1w">
+    <div class="fr-grid-row fr-grid-row--middle fr-grid-row--gutters">
+      <div class="fr-col-md-6 fr-col-12">
+        <img class="fr-responsive-img" :src="consultation.coverUrl" alt="">
+      </div>
+      <div class="fr-col-md-6 fr-col-12">
+        <p class="fr-text--lead">
+          {{ consultation.thematique.picto + " " + consultation.thematique.label }}
+        </p>
+        <h1>{{ consultation.title }}</h1>
+
+        <div v-if="consultation.questionsInfo" class="info-question fr-px-2w fr-py-1w">
+          <div class="fr-mb-1w">
+            <VIcon
+              name="ri-calendar-2-line"
+            />
+            Jusqu'au {{ new Date(consultation.questionsInfo.endDate).toLocaleDateString()}}
+          </div>
+          <div class="fr-mb-1w">
+            <VIcon
+              name="ri-questionnaire-line"
+            />
+            {{ consultation.questionsInfo.questionCount }}
+          </div>
+          <div class="fr-mb-1w">
+            <VIcon
+              name="ri-timer-line"
+            />
+            {{ consultation.questionsInfo.estimatedTime }}
+          </div>
+          <div class="fr-mb-1w">
+            <VIcon
+              name="ri-group-line"
+            />
+            <span class="fr-pl-1v" v-if="consultation.questionsInfo.participantCount == 0">Aucun participant</span>
+            <span class="fr-pl-1v" v-else-if="consultation.questionsInfo.participantCount == 1">
+                1 participant
+              </span>
+            <span class="fr-pl-1v" v-else>
+                {{ consultation.questionsInfo.participantCount }} participants
+              </span>
+            <div class="fr-mt-1w fr-ml-3w">
+              <div class="progress-bar fr-mb-1w">
+                <div class="progress-value" :style="{ width:  (consultation.questionsInfo.participantCount / consultation.questionsInfo.participantCountGoal) *100 + '%' }"></div>
+              </div>
+              Prochain objectif : {{ consultation.questionsInfo.participantCountGoal }} participants !
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-if="consultation.consultationDates" class="consultation-dates fr-mb-4w">
+    <h2 class="fr-text--lead title">Lancement de la consultation</h2>
+    <div class="fr-mb-1w">
+      <VIcon
+        name="ri-calendar-2-line"
+      />
+      <span class="fr-ml-1w">
+          Du {{ new Date(consultation.consultationDates.startDate).toLocaleDateString()}}
+          au {{ new Date(consultation.consultationDates.endDate).toLocaleDateString()}}
+        </span>
+    </div>
+  </div>
+
+  <div v-if="consultation.responsesInfo" class="info-response fr-px-2w fr-py-1w fr-mb-1w">
+      <span class="fr-text--lead">
+        {{ consultation.responsesInfo.picto}}
+      </span>
+    <span class="fr-text--lg fr-ml-1w" v-html="consultation.responsesInfo.description">
+      </span>
+  </div>
+
+  <div v-if="consultation.infoHeader" class="info-header fr-px-2w fr-py-1w fr-mb-1w">
+      <span class="fr-text--lead">
+        {{ consultation.infoHeader.picto}}
+      </span>
+    <span class="fr-text--lg fr-ml-1w" v-html="consultation.infoHeader.description">
+      </span>
+  </div>
+
+  <ConsultationSections :sections="consultation.body.sections"/>
+
+  <DsfrTile
+    v-if="consultation.downloadAnalysisUrl"
+    title="Télécharger la synthèse complète"
+    description="Pour aller plus loin, retrouvez l'analyse détaillée de l'ensemble des réponses à cette consultation."
+    :to="consultation.downloadAnalysisUrl"
+    :download="true"
+    :img-src="svgBook"
+    class="fr-mb-4w"
+  />
+
+  <div v-if="consultation.goals" class="goals fr-p-3w fr-text--sm" >
+    <div class="fr-mt-1v" v-for="goal in consultation.goals">
+      <span class="fr-text--lead fr-mr-2w">{{ goal.picto }}</span>
+      <span v-html="goal.description"></span>
+    </div>
+  </div>
+
+  <div v-if="consultation.footer" class="footer">
+    <h2 class="fr-text--lead title">{{ consultation.footer.title }}</h2>
+    <div v-html="consultation.footer.description"></div>
+  </div>
+</template>
+
+<style>
+.info-header, .info-response{
+  background-color: #f5f7ff;
+  border: 1px solid #c2cefd;
+  border-radius: 10px;
+}
+
+.info-question{
+  .progress-bar{
+    background-color: #dcdcdc;
+    border-radius: 8px;
+    height: 10px;
+
+    .progress-value{
+      background-color: var(--text-title-blue-france);
+      border-radius: 8px;
+      height: 10px;
+
+    }
+  }
+}
+
+.call-to-action{
+  background: var(--background-open-blue-france-hover);
+}
+.ov-icon{
+  color: var(--text-title-blue-france)
+}
+.sections{
+  .section{
+    .section-title{
+      color: var(--text-title-blue-france)
+    }
+  }
+}
+.title{
+  color: var(--text-title-blue-france)
+}
+
+.goals{
+  background-color: #eeeeee;
+}
+</style>
--- client/pages/consultations/[id]/update/[updateId].vue
+++ client/pages/consultations/[id]/update/[updateId].vue
@@ -33,108 +33,7 @@ const { data: consultationUpdate } = await useFetch(routeUrl,{
   <DsfrBreadcrumb :links="links"/>
 
   <div>
-    <div class="fr-grid fr-mb-1w">
-      <div class="fr-grid-row fr-grid-row--middle fr-grid-row--gutters">
-        <div class="fr-col-md-6 fr-col-12">
-          <img class="fr-responsive-img" :src="consultationUpdate.coverUrl" alt="">
-        </div>
-        <div class="fr-col-md-6 fr-col-12">
-          <p class="fr-text--lead">
-            {{ consultationUpdate.thematique.picto + " " + consultationUpdate.thematique.label }}
-          </p>
-          <h1>{{ consultationUpdate.title }}</h1>
-
-          <div v-if="consultationUpdate.questionsInfo" class="info-question fr-px-2w fr-py-1w">
-            <div class="fr-mb-1w">
-              <VIcon
-                name="ri-calendar-2-line"
-              />
-              Jusqu'au {{ new Date(consultationUpdate.questionsInfo.endDate).toLocaleDateString()}}
-            </div>
-            <div class="fr-mb-1w">
-              <VIcon
-                name="ri-questionnaire-line"
-              />
-              {{ consultationUpdate.questionsInfo.questionCount }}
-            </div>
-            <div class="fr-mb-1w">
-              <VIcon
-                name="ri-timer-line"
-              />
-              {{ consultationUpdate.questionsInfo.estimatedTime }}
-            </div>
-            <div class="fr-mb-1w">
-              <VIcon
-                name="ri-group-line"
-              />
-              <span class="fr-pl-1v" v-if="consultationUpdate.questionsInfo.participantCount == 0">Aucun participant</span>
-              <span class="fr-pl-1v" v-else-if="consultationUpdate.questionsInfo.participantCount == 1">
-                1 participant
-              </span>
-              <span class="fr-pl-1v" v-else>
-                {{ consultationUpdate.questionsInfo.participantCount }} participants
-              </span>
-              <div class="fr-mt-1w fr-ml-3w">
-                <div class="progress-bar fr-mb-1w">
-                  <div class="progress-value" :style="{ width:  (consultationUpdate.questionsInfo.participantCount / consultationUpdate.questionsInfo.participantCountGoal) *100 + '%' }"></div>
-                </div>
-                Prochain objectif : {{ consultationUpdate.questionsInfo.participantCountGoal }} participants !
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div v-if="consultationUpdate.consultationDates" class="consultation-dates fr-mb-4w">
-      <h2 class="fr-text--lg title">Lancement de la consultation</h2>
-      <div class="fr-mb-1w">
-        <VIcon
-          name="ri-calendar-2-line"
-        />
-        Du {{ new Date(consultationUpdate.consultationDates.startDate).toLocaleDateString()}}
-        au {{ new Date(consultationUpdate.consultationDates.endDate).toLocaleDateString()}}
-      </div>
-    </div>
-
-    <div v-if="consultationUpdate.responsesInfo" class="info-response fr-px-2w fr-py-1w fr-mb-1w">
-      <span class="fr-text--lead">
-        {{ consultationUpdate.responsesInfo.picto}}
-      </span>
-      <span class="fr-text--lg" v-html="consultationUpdate.responsesInfo.description">
-      </span>
-    </div>
-
-    <div v-if="consultationUpdate.infoHeader" class="info-header fr-px-2w fr-py-1w fr-mb-1w">
-      <span class="fr-text--lead">
-        {{ consultationUpdate.infoHeader.picto}}
-      </span>
-      <span class="fr-text--lg" v-html="consultationUpdate.infoHeader.description">
-      </span>
-    </div>
-
-    <ConsultationSections :sections="consultationUpdate.body.sections"/>
-
-    <DsfrTile
-      v-if="consultationUpdate.downloadAnalysisUrl"
-      title="Télécharger la synthèse complète"
-      description="Pour aller plus loin, retrouvez l'analyse détaillée de l'ensemble des réponses à cette consultation."
-      :to="consultationUpdate.downloadAnalysisUrl"
-      :download="true"
-      :img-src="svgBook"
-      class="fr-mb-4w"
-    />
-
-    <div v-if="consultationUpdate.goals" class="goals" >
-      <div class="fr-mt-1v" v-for="goal in consultationUpdate.goals">
-        <span class="fr-text--lead fr-mr-2w">{{ goal.picto }}</span>
-        <span v-html="goal.description"></span>
-      </div>
-    </div>
-
-    <div v-if="consultationUpdate.footer" class="footer">
-      <h2 class="fr-text--lg title">{{ consultationUpdate.footer.title }}</h2>
-      <div v-html="consultationUpdate.footer.description"></div>
-    </div>
+    <ConsultationContent :consultation="consultationUpdate" />
 
     <ConsultationHistory v-if="consultationUpdate.history" :history="consultationUpdate.history"  :consultation-id="consultationId" :current-update-id="consultationUpdateId" class="fr-mt-6w"/>
 
@@ -153,47 +52,11 @@ const { data: consultationUpdate } = await useFetch(routeUrl,{
 </template>
 
 <style>
-.info-header, .info-response{
-  background-color: #f5f7ff;
-  border: 1px solid #c2cefd;
-  border-radius: 10px;
-}
 
-.info-question{
-  .progress-bar{
-    background-color: #dcdcdc;
-    border-radius: 8px;
-    height: 10px;
-
-    .progress-value{
-      background-color: var(--text-title-blue-france);
-      border-radius: 8px;
-      height: 10px;
-
-    }
-  }
-}
-
-.call-to-action{
-  background: var(--background-open-blue-france-hover);
-}
-.ov-icon{
-  color: var(--text-title-blue-france)
-}
-.sections{
-  .section{
-    .section-title{
-      color: var(--text-title-blue-france)
-    }
-  }
-}
 .feedback-question{
   .feedback-question-title{
     color: var(--text-title-blue-france)
   }
-}
-.title{
-  color: var(--text-title-blue-france)
 }
 
 </style>
